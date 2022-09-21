22c788e0e495558146afa61e5d5567fe652f79c7
Villera69 <william.ramsfeldt@gmail.com>
2022-09-21 14:46:37 +0200

    Done with the requested functions of the game. Added 3 classes, player, program and goomba.

--- BashAMonster/Program.cs
+++ BashAMonster/Program.cs
@@ -1,1 +1,36 @@
 ﻿
+namespace BashAMonster {
+class Goomba{
+    public int hp;
+
+    public Goomba (int hp){
+        this.hp = hp;
+    }
+
+}
+
+class Player{
+
+    string name;
+
+    public Player(string name){
+        this.name = name;
+    }
+}
+
+class Program{
+        static void Main(){
+        Goomba Goomba1 = new Goomba(100);
+        Console.WriteLine("The Goomba has " + (Goomba1.hp) + "hp. Press enter to hit it.");
+        Console.ReadLine();
+        Goomba1.hp =- 20;
+        Console.WriteLine("The Goomba now has " + (Goomba1.hp) + "hp");
+        Console.ReadLine();
+
+    }
+    
+
+}
+}
+
+
